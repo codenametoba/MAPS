094187dae8bba8753b3d904cbd07901b2053c5ec
codenametoba <121884590+codenametoba@users.noreply.github.com>
2023-07-24 16:51:14 +0100

    Add files via upload

---
+++ googlemap-master/map/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Maps from './map';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Maps/>
+      <button className="btn btn-primary">Click me</button>
+
+    </div>
+  );
+}
+
+export default App;
